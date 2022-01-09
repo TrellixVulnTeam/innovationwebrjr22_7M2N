216c45a09fbdca4976de44eac1117b8071c596b8
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2022-01-08 20:06:07 -0600

    Borre archivos.

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -1,7 +1,5 @@
 /*eslint-disable*/
 import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
 // react components for routing our app without refresh
 import { Link } from "react-router-dom";
 
@@ -11,11 +9,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
 
-// @material-ui/icons
-import { MailOutline, Dashboard } from "@material-ui/icons";
-
 // core components
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
 import Button from "components/CustomButtons/Button.js";
 
 import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
@@ -27,43 +21,6 @@ export default function HeaderLinks(props) {
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
-        <CustomDropdown
-          noLiPadding
-          buttonText="Menu"
-          buttonProps={{
-            className: classes.navLink,
-            color: "transparent",
-          }}
-          buttonIcon={Dashboard}
-          dropdownList={[
-            <Link to="/login-page" className={classes.dropdownLink}>
-              Inicia Sesión
-            </Link>,
-            <Link to="/login-page" className={classes.dropdownLink}>
-              Registro
-            </Link>,
-            <Link to="/" className={classes.dropdownLink}>
-              Inicio
-            </Link>
-          ]}
-        />
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Button
-          href="/landing-page"
-          color="transparent"
-          className={classes.navLink}
-        >
-          <MailOutline className={classes.icons} /> Contacto
-        </Button>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        {/*<Tooltip title="Delete">
-          <IconButton aria-label="Delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>*/}
-
         <Tooltip
           id=""
           title="LinkedIn"
